05c5de810f71acc97251b083694dfe87c262c0d2
quythanh <quythanh.dev@gmail.com>
2025-05-12 14:17:04 +0700

    feat: create new code base

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dailydate: resolve(__dirname, 'dailydate.html'),
+                live2dv3: resolve(__dirname, 'live2dv3.html'),
+            }
+        }
+    }
+})
