6a05ff09f12e3dfe2cf30fdd19dd5768ae302cc4
quythanh <quythanh.dev@gmail.com>
2025-06-03 16:45:28 +0700

    fix: vite build, use plugin to copy live2dcubismcore libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   appType: 'mpa',
@@ -11,5 +12,18 @@ export default defineConfig({
         live2dv3: resolve(__dirname, 'live2dv3.html'),
       }
     }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@hazart-pkg/live2d-core/live2dcubismcore.min.js',
+          dest: 'assets/js/libs'
+        }
+      ]
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
   }
 })
